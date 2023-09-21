e689614fbb9f36ef1bdb41c71f04293cc678fa74
DebuggerX <dx8917312@163.com>
2023-09-21 17:38:14 +0800

    update distro hint.

--- fcitx5_customizer.sh
+++ fcitx5_customizer.sh
@@ -109,8 +109,8 @@ if ! check_installed fcitx5 || [ "$GTK_IM_MODULE" != "fcitx" ] ; then
   exit
 fi
 
-if ! [ -e /usr/bin/apt ] ; then
-  dialog --msgbox "目前本脚本只能在debian系(deepin、ubuntu)发行版中运行" 10 32
+if [ -e /usr/bin/apt ] ; then
+  dialog --msgbox "目前本脚本只能在 debian 系列 (deepin、ubuntu、mint等)发行版中运行" 10 32
   exit
 fi
 
